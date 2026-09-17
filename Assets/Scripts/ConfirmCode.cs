463a6306c41c092cb3e5336899f7daa524fb6afe
Insensato <insensato.univ@gmail.com>
2026-09-17 03:02:10 -0300

    Cambios para poder Buildear + Perfil Web

--- Assets/Scripts/ConfirmCode.cs
+++ Assets/Scripts/ConfirmCode.cs
@@ -1,4 +1,3 @@
-using Mono.Cecil.Cil;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
